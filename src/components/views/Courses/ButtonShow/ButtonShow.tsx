2d7976d934c9d48fb1535bf81d3cb5167ad44172
Shpm21 <galleguilloscristian2@gmail.com>
2022-11-25 07:31:46 -0400

    add variable to message button

--- src/components/views/Courses/ButtonShow/ButtonShow.tsx
+++ src/components/views/Courses/ButtonShow/ButtonShow.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import './ButtonShow.css'
 interface Props {
   handleButtonAsignatures: (event: any) => Promise<void>
+  isVisible: boolean
 }
 
-const ButtonShow: React.FC<Props> = ({ handleButtonAsignatures }) => {
+const ButtonShow: React.FC<Props> = ({
+  handleButtonAsignatures,
+  isVisible
+}) => {
+  const message = isVisible ? 'Ocultar asignaturas' : 'Mostrar asignaturas'
   return (
     <>
       <div className="button-container-show">
         <div className="button-show">
           <button className="button-EBL" onClick={handleButtonAsignatures}>
-            Mostrar asignaturas
+            {message}
           </button>
         </div>
       </div>
